4b75b11f41d475a8d1c5b505e3fad98a0bd7bac4
Alvianto21 <deliverindihom@gmail.com>
2025-06-02 18:36:24 +0700

    fix typo

--- resources/views/password/forget.blade.php
+++ resources/views/password/forget.blade.php
@@ -35,7 +35,7 @@
 	</section>
 
 	@push('scripts')
-        <!-- alpine js -->
+    <!-- alpine js -->
      <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @endpush
 
