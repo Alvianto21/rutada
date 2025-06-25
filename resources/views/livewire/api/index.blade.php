f0002907dbe03e88a7f829f740af1436c1d5648d
Alvianto21 <alviantovinsensius@gmail.com>
2025-06-25 19:19:25 +0700

    add alert component

--- resources/views/livewire/api/index.blade.php
+++ resources/views/livewire/api/index.blade.php
@@ -1,7 +1,7 @@
 <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+    <x-alert></x-alert>
     <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-
             <!-- Actions Bar -->
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div
